53ef5173e613485cb1c1542a79a8d76530ccbff7
achristson <christsona0@outlook.com>
2022-02-12 17:49:02 -0600

    Dice Roller app with two dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button =  findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+    }
+
+    private fun rollDice() {
+        val randomInt1 = Random().nextInt(7)
+        val randomInt2 = Random().nextInt(7)
+
+        diceImage1.setImageResource(getDrawableImage(randomInt1))
+        diceImage2.setImageResource(getDrawableImage(randomInt2))
+    }
+
+    private fun getDrawableImage(randomInt: Int): Int{
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableResource
     }
 }
